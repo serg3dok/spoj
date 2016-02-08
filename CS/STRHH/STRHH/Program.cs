6d6d8d7144d50489cd91763cbf7ae39f703c8625
Sergey Okinchuk <serg3d@hotmail.com>
2016-02-08 13:53:36 -0800

    Added C# projects

---
+++ CS/STRHH/STRHH/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace STRHH
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = Int32.Parse(Console.ReadLine());
+            for (int i = 0; i < n; i++)
+            {
+                char
+            }
+        }
+    }
+}
